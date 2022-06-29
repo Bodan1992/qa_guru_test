47686067a51b30b0c2363c60af547cc9a009c1a1
Bodan1992 <bodan1992@gmail.com>
2022-06-29 14:25:07 +0300

    made changes to the code in the conftest and test_practice_form files

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,60 +1,67 @@
 from selene.support.shared import browser
-from selene import be, have, command
+from selene import have, command
+import os
 
-def opened_form():
+def arrange_form_opened():
     browser.open('/automation-practice-form')
     browser.all(
         '[id^=google_ads][id$=container__]').with_(timeout=10).should(have.size(3)).perform(command.js.remove)
 
 
-def test_form():
-    opened_form()
-    # browser.open('/automation-practice-form')
+def test_register_form():
+    arrange_form_opened()
 
-    browser.element('#firstName').should(be.blank).type('Bohdan')
-    browser.element('#lastName').should(be.blank).type('Obruch')
-    browser.element('#userEmail').should(be.blank).type('bodan@gmail.com')
-    browser.element('[for="gender-radio-1"]').click()
-    browser.element('#userNumber').should(be.blank).type('0960263611')
-    browser.element('#dateOfBirthInput').scroll_to().click()
-    browser.element('[value="1992"]').click()
-    browser.element('[value="0"]').click()
-    browser.element('div[aria-label="Choose Wednesday, January 1st, 1992"]').click()
-    browser.element('#subjectsInput').should(be.blank).type('Maths').press_tab()
-    browser.element('#subjectsInput').should(be.blank).type('English').press_tab()
-    browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element('[for="hobbies-checkbox-3"]').click()
-    browser.element('#uploadPicture').type('C:/Users/bohda/PycharmProjects/qa_guru-python/demoqa-test/tests/photo.jpg')
-    browser.element('#currentAddress').should(be.blank).type('г.Киев, ул.Академика Туполева 20в')
-    browser.element('#react-select-3-input').type('Haryana').press_tab()
-    browser.element('#react-select-4-input').type('Karnal').press_tab().press_enter()
-    #browser.element('#submit').click()
+    # Act
+    browser.element('#firstName').type('Bohdan')
 
-    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
-    browser.elements("table tr").element(1).should(have.text('Bohdan Obruch'))
-    browser.elements("table tr").element(2).should(have.text('bodan@gmail.com'))
-    browser.elements("table tr").element(3).should(have.text('Male'))
-    browser.elements("table tr").element(4).should(have.text('0960263611'))
-    browser.elements("table tr").element(5).should(have.text('01 January,1992'))
-    browser.elements("table tr").element(6).should(have.text('Maths, English'))
-    browser.elements("table tr").element(7).should(have.text('Sports, Music'))
-    browser.elements("table tr").element(8).should(have.text('photo.jpg'))
-    browser.elements("table tr").element(9).should(have.text('г.Киев, ул.Академика Туполева 20в'))
-    browser.elements("table tr").element(10).should(have.text('Haryana Karnal'))
+    browser.element('#lastName').type('Obruch')
 
-    # browser.elements('table tr').element(i).should(have.exact_texts(
-    #     'Bohdan Obruch',
-    #     'bodan@gmail.com',
-    #     'Male',
-    #     '0960263611',
-    #     '01 January,1992',
-    #     'Maths, English',
-    #     'Sports, Music',
-    #     'photo.jpg',
-    #     'г.Киев, ул.Академика Туполева 20в',
-    #     'Haryana Karnal'))
+    browser.element('#userEmail').type('bodan@gmail.com')
 
-#scroll_to.
-# .perform(command.js.scroll_into_view)
+    gender_group = browser.element('#genterWrapper')
+    gender_group.all('.custom-radio').element_by(have.exact_text("Male")).click()
 
-# browser.element('#uploadPicture').type('bohda\PycharmProjects\Automation_qa_guru\photo.jpg')
+    browser.element('#userNumber').type('0960263611')
+
+    browser.element('#dateOfBirthInput').perform(command.js.scroll_into_view).click()#
+    browser.element('.react-datepicker__year-select').element('[value="1992"]').click()
+    browser.element('.react-datepicker__month-select').element('[value="0"]').click()
+    browser.element('.react-datepicker__day--001').click()
+
+    browser.element('#subjectsInput').type('Maths').press_tab()
+    browser.element('#subjectsInput').type('English').press_tab()
+
+    browser.element('#hobbiesWrapper').all('.custom-checkbox').element_by(have.exact_text('Sports')).click()
+    browser.element('#hobbiesWrapper').all('.custom-checkbox').element_by(have.exact_text('Music')).click()
+
+    browser.element('#uploadPicture').send_keys(os.path.abspath('../resources/photo.jpg'))
+
+    browser.element('#currentAddress').type('г.Киев, ул.Академика Туполева 20в')
+
+    browser.element('#state').element('input').type('Haryana').press_tab()
+    browser.element('#city').element('input').type('Karnal').press_tab()
+
+    browser.element('#submit').perform(command.js.click)
+
+    #Assert
+    browser.element('.modal-dialog').element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+
+    browser.element('.modal-dialog').all("table tr")[1].all('td').should(have.exact_texts('Student Name','Bohdan Obruch'))
+
+    browser.element('.modal-dialog').all("table tr")[2].all('td').should(have.exact_texts('Student Email', 'bodan@gmail.com'))
+
+    browser.element('.modal-dialog').all("table tr")[3].all('td').should(have.exact_texts('Gender', 'Male'))
+
+    browser.element('.modal-dialog').all("table tr")[4].all('td').should(have.exact_texts('Mobile', '0960263611'))
+
+    browser.element('.modal-dialog').all("table tr")[5].all('td').should(have.exact_texts('Date of Birth', '01 January,1992'))
+
+    browser.element('.modal-dialog').all("table tr")[6].all('td').should(have.exact_texts('Subjects', 'Maths, English'))
+
+    browser.element('.modal-dialog').all("table tr")[7].all('td').should(have.exact_texts('Hobbies','Sports, Music'))
+
+    browser.element('.modal-dialog').all("table tr")[8].all('td').should(have.exact_texts('Picture', 'photo.jpg'))
+
+    browser.element('.modal-dialog').all("table tr")[9].all('td').should(have.exact_texts('Address', 'г.Киев, ул.Академика Туполева 20в'))
+
+    browser.element('.modal-dialog').all("table tr")[10].all('td').should(have.exact_texts('State and City', 'Haryana Karnal'))
